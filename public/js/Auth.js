bc7b920e2b27470616de123e60eb94366d93127d
ItsPitxh <pitched.10121@gmail.com>
2021-11-19 20:41:38 +0700

    third commit

--- public/js/Auth.js
+++ public/js/Auth.js
@@ -1,40 +1,70 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getAuth , createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-var firebaseui = require('firebaseui');
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBimKQ9uh2nmMMQLsk5Xl60m-O07d23wnw",
+  authDomain: "projectlongmon.firebaseapp.com",
+  databaseURL: "https://projectlongmon-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "projectlongmon",
+  storageBucket: "projectlongmon.appspot.com",
+  messagingSenderId: "453457350228",
+  appId: "1:453457350228:web:e39c0f390516382b9ba7c1",
+  measurementId: "G-S17KNGMLHQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+
 
 
 // const auth = firebase.auth();
 
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-});
+// ui.start('#firebaseui-auth-container', {
+//   signInOptions: [
+//     auth.EmailAuthProvider.PROVIDER_ID,
+//     auth.GoogleAuthProvider.PROVIDER_ID
+//   ],
+//   // Other config options...
+// });
 
 function register() {
-  var email = document.querySelector("#signup-email").value;
+  var email = document.querySelector("#signup-email").value; //input 
   var password = document.querySelector("#signup-password").value;
-  auth.createUserWithEmailAndPassword(email, password).then(function(user) {
-       var user = firebase.auth().currentUser;
-       console.log(user); // Optional
-    }, function(error) {
-       // Handle Errors here.
-       var errorCode = error.code;
-       var errorMessage = error.message;
-
-       window.alert(errorMessage)
-    });
+  auth.createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorMessage);
+  });
 }
 
 function login(){
   var email = document.getElementById("login_email").value;
   var password = document.getElementsById("login_password").value;
 
-  const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-  promise.catch(e => alert(e.message));
-
-  alert("Signed Up" + email);
-        
-    
+  auth.signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorMessage);
+  });   
 }
